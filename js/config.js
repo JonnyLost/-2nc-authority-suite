8211e6f4239ea85dd91ff486534d6c80695de481
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-07-31 07:57:05 -0400

    Release v4.8: update js/config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = Object.freeze({
   name: '2NC Authority Suite',
-  version: '4.7.0',
-  build: '2026-07-31-comic-label-preferences',
+  version: '4.8.0',
+  build: '2026-07-31-marvel-authority-expansion',
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.7.0',
-  expectedMinimums: { music: 4500, comic: 2500 },
+  cacheName: '2nc-authority-suite-v4.8.0',
+  expectedMinimums: { music: 4500, comic: 3300 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
