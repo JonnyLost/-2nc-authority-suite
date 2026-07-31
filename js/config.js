8527f14ea6648c3428310ccff94092949bd32cdd
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-07-31 10:41:55 -0400

    Add comic authority/series filter (js/config.js)

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = Object.freeze({
   name: '2NC Authority Suite',
-  version: '4.9.0',
-  build: '2026-07-31-dc-deep-authority-workflow',
+  version: '4.9.1',
+  build: '2026-07-31-comic-authority-series-filter',
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.9.0',
+  cacheName: '2nc-authority-suite-v4.9.1',
   expectedMinimums: { music: 4500, comic: 3550 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
