031060adde396798ff8057641aef5d0b44fbe057
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-07-30 22:56:20 -0400

    Fix iOS safe-area background

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = Object.freeze({
   name: '2NC Authority Suite',
-  version: '4.5.0',
+  version: '4.5.1',
   build: '2026-07-30-dc-publishing-lines',
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.5.0',
+  cacheName: '2nc-authority-suite-v4.5.1',
   expectedMinimums: { music: 4500, comic: 2500 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
