c9701e6c9f269b3c6e0887de7caaa79622602695
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-08-01 17:47:15 -0400

    Configure v4.12 data bundle

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = Object.freeze({
   name: '2NC Authority Suite',
-  version: '4.11.1',
-  build: '2026-08-01-quick-lookup-add-to-queue',
+  version: '4.12.0',
+  build: '2026-08-01-marvel-team-authority-audit',
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.11.1',
-  expectedMinimums: { music: 4500, comic: 3790 },
+  cacheName: '2nc-authority-suite-v4.12.0',
+  expectedMinimums: { music: 4500, comic: 3920 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
