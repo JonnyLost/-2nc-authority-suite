5a656676d0cabab2c5479ef02c05ca5ba520889f
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-08-01 13:37:57 -0400

    Prepare Authority Suite v4.10

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window.APP_CONFIG = Object.freeze({
   name: '2NC Authority Suite',
-  version: '4.9.1',
-  build: '2026-07-31-comic-authority-series-filter',
+  version: '4.10.0',
+  build: '2026-08-01-marvel-event-authority-expansion',
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.9.1',
-  expectedMinimums: { music: 4500, comic: 3550 },
+  cacheName: '2nc-authority-suite-v4.10.0',
+  expectedMinimums: { music: 4500, comic: 3790 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
