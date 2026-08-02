5b09bef3843d2543d0cf134864061a4083382b16
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-08-02 07:20:54 -0400

    Validate v4.16 Aliens filing authority

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ window.APP_CONFIG = Object.freeze({
   schema: 3,
   databaseName: '2nc-authority-db-v3',
   legacyDatabaseNames: ['2nc-authority-db-v2-5'],
-  cacheName: '2nc-authority-suite-v4.16.0',
+  cacheName: '2nc-authority-suite-v4.16.0-r1',
   expectedMinimums: { music: 4500, comic: 5200 },
   bundledFiles: { music: 'data/music.json', comic: 'data/comics.json' }
 });
