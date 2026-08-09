6c3c2d0c9b2cb5c076bb3f78c82fc82408619494
JonnyLost <5grwkpwtyr@privaterelay.appleid.com>
2026-08-09 13:44:46 -0400

    Refresh Authority offline cache for v4.28.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = '2nc-authority-suite-v4.28.0';
+const CACHE = '2nc-authority-suite-v4.28.1';
 const CORE = [
   './','./index.html','./404.html','./styles.css','./app.js','./manifest.webmanifest','./VERSION.json',
   './js/config.js','./js/logger.js','./js/db.js','./js/sync.js','./js/print-station.js','./js/pdf.js','./js/labels.js','./js/ui.js',
